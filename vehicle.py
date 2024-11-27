e7cf6f1669e04ec8ca049ef38994654ddf3c29e8
SUKI S <sukielectrotech@gmail.com>
2024-11-27 22:45:42 +0530

    Update vehicle.py

--- vehicle.py
+++ vehicle.py
@@ -4,22 +4,16 @@ class Vehicle:
         self.make = make
         self.yearofmanufacture = yearofmanufacture
         self.value = value
-
     def getregnumber(self) -> str:
         return self.regno
-
     def getmake(self) -> str:
         return self.make
-
     def getyear(self) -> int:
         return self.yearofmanufacture
-
     def getvalue(self) -> float:
         return self.value
-
     def setvalue(self, value: float):
         self.value = value
-
     def calculateage(self, current_year: int) -> int:
         return current_year - self.yearofmanufacture
 
@@ -27,10 +21,8 @@ class SecondHandVehicle(Vehicle):
     def __init__(self, regno: str, make: str, yearofmanufacture: int, value: float, numberofowners: int):
         super().__init__(regno, make, yearofmanufacture, value)
         self.numberofowners = numberofowners
-
     def getnumberofowners(self) -> int:
         return self.numberofowners
-
     def sellvehicle(self, new_value: float):
         self.setvalue(new_value)
         self.numberofowners += 1
